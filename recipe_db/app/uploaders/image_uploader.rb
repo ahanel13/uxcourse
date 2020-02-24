633c9c280b71ff8519c21b82038127b905e7ff72
Anthony Hanel <ahanel13@gmail.com>
2020-02-24 16:03:42 -0600

    added storage:fog and carrier wave

--- recipe_db/app/uploaders/image_uploader.rb
+++ recipe_db/app/uploaders/image_uploader.rb
@@ -1,16 +1,16 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # storage :file
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
+  # def store_dir
+  #   "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  # end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url(*args)
+  # def default_url( *args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
